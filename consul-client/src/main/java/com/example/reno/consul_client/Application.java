035638c8f065cf50c2d9faaa3725ef65cc59abdb
Trist <1223562701@qq.com>
2017-07-30 23:46:06 +0800

    服务的注册与发现

---
+++ consul-client/src/main/java/com/example/reno/consul_client/Application.java
@@ -0,0 +1,14 @@
+package com.example.reno.consul_client;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+}
